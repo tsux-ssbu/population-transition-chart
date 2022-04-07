ba9be4404691a6c03d31d7600583fcc8517826d3
tsux-ssbu <tomoya18xxx@gmail.com>
2022-04-07 20:16:20 +0900

    add: スタイルつけた #1

--- src/components/CheckBoxItem/index.tsx
+++ src/components/CheckBoxItem/index.tsx
@@ -1,5 +1,7 @@
 import { memo, VFC } from 'react';
+
 import { PrefectureType } from 'src/types/prefecture';
+import styles from './CheckboxItem.module.css';
 
 type Props = {
   prefecture: PrefectureType;
@@ -9,11 +11,13 @@ export const CheckboxItem: VFC<Props> = memo((props) => {
   const { prefecture } = props;
 
   return (
-    <label>
-      <input type='checkbox' name='prefectures' />
-      <span>
-        {prefecture.prefName.length === 3 ? `${prefecture.prefName}　` : `${prefecture.prefName}`}
-      </span>
-    </label>
+    <div>
+      <label className={styles.checkbox}>
+        <input type='checkbox' name='prefectures' />
+        <span>
+          {prefecture.prefName.length === 3 ? `${prefecture.prefName}　` : `${prefecture.prefName}`}
+        </span>
+      </label>
+    </div>
   );
 });
--- src/components/CheckboxList/index.tsx
+++ src/components/CheckboxList/index.tsx
@@ -1,6 +1,8 @@
 import { memo, VFC } from 'react';
+
 import { PrefectureType } from 'src/types/prefecture';
-import { CheckboxItem } from '../CheckBoxItem';
+import { CheckboxItem } from 'src/components/CheckboxItem';
+import styles from './CheckboxList.module.css';
 
 type Props = {
   prefectures: PrefectureType[] | undefined;
@@ -10,9 +12,9 @@ export const CheckBoxList: VFC<Props> = memo((props) => {
   const { prefectures } = props;
 
   return (
-    <div>
-      <h3>都道府県</h3>
-      <div>
+    <div className={styles.container}>
+      <h3 className={styles.heading}>都道府県</h3>
+      <div className={styles.checkbox_list}>
         {prefectures?.map((prefecture) => {
           return <CheckboxItem key={prefecture.prefCode} prefecture={prefecture} />;
         })}
